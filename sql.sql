3fefa569362f1b787dac757a623a6de972cd19b7
frsv14 <50055688+frsv14@users.noreply.github.com>
2024-05-23 10:12:50 +0200

    Create sql.sql
    some clarification to how to use the db aspect

---
+++ sql.sql
@@ -0,0 +1,11 @@
+-- detta är vad du behöver för att kunna köra servern
+-- 1. skapa en användare i din sql terminal. (om du redan har en med alla behörigheter)
+-- 2. Skriv in alla uppgfter för databasen i varibeln db
+-- 3. skapa databasen via commandot:
+CREATE DATABASE databas;
+-- 4. skapa de två tabellerna som behövs:
+CREATE TABLE msg (msg TEXT, created DATE, user TEXT);
+CREATE TABLE user (name TEXT, password TEXT);
+-- 5. create a user so you can loggin på hemsidan
+INSERT INTO user VALUES (YOUR_NAME , YOUR_PASSWORD);
+-- 6. HAVE FUN
